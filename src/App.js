06092c6d302f04ac73f1bce238dddbd5659ce545
乐天居易 <19836788252@163.com>
2024-04-17 11:00:52 +0800

    JSX基础-实现基础的条件渲染

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
-// 
-const list = [
-  {id:1001, name:'Vue'},
-  {id:1002, name: 'React'},
-  {id:1003, name: 'Angular'}
-]
+// JSX基础-实现基础的条件渲染
+const isLogin  = false
 function App() {
   return (
     <div className="App">
-      this is App
-    {/* 渲染列表*/}
-    {/* map 循环哪个结构 return结构*/}
-    {/*注意事项：加上一个独一无二的key 字符串或者number id*/}
-    {/*key的作用：React框架内部使用 提升更新性能的*/}
-    {list.map(item=><li key={item.id}>{item.name}</li>)}      
-
+      {isLogin && <span>this is span</span>}
+      {isLogin ? <div>jack</div>:<div>loading...</div> }
     </div>
   );
 }
@@ -45,4 +36,26 @@ export default App;
 //     </div>
 //   );
 // }
+// export default App;
+
+// JSX基础-实现列表渲染
+// const list = [
+//   {id:1001, name:'Vue'},
+//   {id:1002, name: 'React'},
+//   {id:1003, name: 'Angular'}
+// ]
+// function App() {
+//   return (
+//     <div className="App">
+//       this is App
+//     {/* 渲染列表*/}
+//     {/* map 循环哪个结构 return结构*/}
+//     {/*注意事项：加上一个独一无二的key 字符串或者number id*/}
+//     {/*key的作用：React框架内部使用 提升更新性能的*/}
+//     {list.map(item=><li key={item.id}>{item.name}</li>)}      
+
+//     </div>
+//   );
+// }
+
 // export default App;
