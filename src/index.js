e2408c491772a23951c6aa0bea35c7cfb1c831e4
乐天居易 <19836788252@163.com>
2024-04-17 10:02:38 +0800

    React开发环境创建

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// 项目的入口从这里开始运行
+
+// React必要的两个核心包
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// 导入项目的根组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+// 把App根组件渲染到id为root的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
