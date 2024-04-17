e1a3a2c85ca60078ce9f47fdb759ed8dcd87e1fa
乐天居易 <19836788252@163.com>
2024-04-17 10:24:32 +0800

    JSX基础-识别js表达式~

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
+const count = 100
+const getName=()=>{
+  return 'jack'
+}
 function App() {
   return (
     <div className="App">
      this is App
+     {/* 使用引号传递字符串 */}
+     {' this is message'}
+     {/* 使用JavaScript变量 */}
+     {count}
+     {/* 函数调用 */}
+     {getName()}
+     {/* 方法调用 */}
+     {new Date().getDate()}
+     {/* 使用JavaScript对象 */}
+     <div style={{color:'red'}}>this is div</div>
     </div>
   );
 }
