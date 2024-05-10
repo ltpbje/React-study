978b9eada89d3f0919d58952cc9903da73ed69f1
乐天居易 <19836788252@163.com>
2024-05-10 17:23:51 +0800

    组件通信-父传子-基础实现

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 // 项目的根组件
 // App -> index.js -> public/index.html(root)
-// React中获取DOM
-
-import { useRef } from "react"
 
 
-// 1.useRef生成ref对象绑定到dom标签身上
-//2.dom可用时，ref.current获取dom
-//渲染完毕之后dom生成之后才可用
+//  父传子
+//  1.父组件传递数据 子组件标签身上绑定属性
+//  2.子组件接收数据 props的参数
 
-
+function Son(props){
+  // props：对象里面包含了父组件传递过来的所有的数据
+  console.log(props)
+  return (
+    <div>this is son,{props.name}</div>
+  )
+}
 
 function App(){
-
-  const inputRef = useRef(null)
-  function showDom (){
-    console.dir(inputRef.current)
-  }
+  const name ='this is a app name'
   return (
     <div>
-      <input type='text' ref={inputRef}></input>
-      <button onClick={showDom}>获取dom</button>
+      <Son name={name} ></Son>
     </div>
   ) 
 }
