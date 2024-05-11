9e273b1e92877fa8fcf4b575dbee0087b9a4b56e
乐天居易 <19836788252@163.com>
2024-05-11 09:16:18 +0800

    使用状态提升实现兄弟组件通信

--- src/App.js
+++ src/App.js
@@ -2,39 +2,36 @@
 // App -> index.js -> public/index.html(root)
 
 import { useState } from "react"
+// 使用状态提升实现兄弟组件通信
+function A({onGetAMsg}){
+  // Son组件当中的数据
+  const name = 'this is A name'
+  return (
+   <div>
+     this is A component
+     <button onClick={()=>onGetAMsg(name)}>sendMsg</button>
+   </div>
 
-
-
-//  父传子
-//  1.父组件传递数据 子组件标签身上绑定属性
-//  2.子组件接收数据 props的参数
-
-function Son(props){
-  // Son组件中的数据
-  const sonMsg = 'this is son msg'
+  )
+}
+function B(props){
   return (
     <div>
-      this is son 
-    <button onClick={()=>props.onGetSonMsg(sonMsg)}>sendMsg</button>      
+      {/* 接受兄弟组件的数据 */}
+      this is B component,{props.name}
     </div>
   )
 }
 function App(){
-  // 这种写法会引起视图上的改变
-  const [msg,setMsg] = useState()
-  // 这种写法不会引起视图上的改变
-  // let msga ='1'
-  const getSonMsg =(msg)=>{
-    console.log(msg)
-    setMsg(msg)
-    // msga = msg
-    // console.log(1)
+  const [name,setName] = useState('')
+  const getAMsg=(msg)=>{
+    setName(msg)
   }
   return (
     <div>
-        this is App,
-        {msg}
-        <Son onGetSonMsg={getSonMsg}></Son>
+        this is App,{name}
+        <A onGetAMsg={getAMsg}></A>
+        <B name={name}></B>
     </div>
   ) 
 }
