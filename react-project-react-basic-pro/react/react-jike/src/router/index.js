f1d61cce7b0d06c8d8c22d7068c265ea5ef3eb36
乐天居易 <19836788252@163.com>
2024-05-30 10:51:09 +0800

    配置别名路径

--- react-project-react-basic-pro/react/react-jike/src/router/index.js
+++ react-project-react-basic-pro/react/react-jike/src/router/index.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import Layout from "../pages/Layout";
-import Login from "../pages/Login";
+import Layout from "@/pages/Layout";
+import Login from "@/pages/Login";
 
 const router = createBrowserRouter([
     {
