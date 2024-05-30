d962cf0ff55b49e79baddbd26982e1e8860fafe7
乐天居易 <19836788252@163.com>
2024-05-30 11:18:37 +0800

    登录-准备静态结构

--- react-project-react-basic-pro/react/react-jike/src/pages/Login/index.js
+++ react-project-react-basic-pro/react/react-jike/src/pages/Login/index.js
@@ -1,7 +1,29 @@
-function Login(){
-return(
-    <div>this is login</div>
-)
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
+const Login = () => {
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
