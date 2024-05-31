bab371478ad092fbb29cf2e3a59aa0c5af40eb86
乐天居易 <19836788252@163.com>
2024-05-31 20:48:50 +0800

    登录-redux管理token-实现登录后续逻辑

--- react-project-react-basic-pro/react/react-jike/src/pages/Login/index.js
+++ react-project-react-basic-pro/react/react-jike/src/pages/Login/index.js
@@ -1,14 +1,21 @@
 import './index.scss'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, message } from 'antd'
 import logo from '@/assets/logo.png'
 import { useDispatch } from 'react-redux'
 import {fetchLogin} from '@/store/modules/user' 
+import { useNavigate } from 'react-router-dom'
 const Login = () => {
+    const navigate = useNavigate()
     const dispatch =useDispatch()
-    const onFinish=(value)=>{   
+    const onFinish=async (value)=>{   
         console.log(value)
         //触发异步action fetchLogin
-        dispatch(fetchLogin(value))
+       await dispatch(fetchLogin(value))
+        // 1.跳转到首页
+        navigate('/')
+        // 2.提示一下用户
+        message.success('登录成功')
+
 
     }
   return (
