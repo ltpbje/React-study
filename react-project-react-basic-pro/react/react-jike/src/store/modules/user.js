653421f950b3e8cdc6367fe505d98225113c6423
乐天居易 <19836788252@163.com>
2024-05-31 21:04:28 +0800

    登录-Token持久化

--- react-project-react-basic-pro/react/react-jike/src/store/modules/user.js
+++ react-project-react-basic-pro/react/react-jike/src/store/modules/user.js
@@ -5,12 +5,14 @@ const userStore= createSlice({
     name:'user',
     // 数据状态
     initialState:{
-        token:''
+        token:localStorage.getItem('token_key') || ''
     },
     // 同步修改方法
     reducers:{
         setToken(state,action){
             state.token = action.payload
+            // localstorage存一份
+            localStorage.setItem('token_key',action.payload)
         }
     }
 })
