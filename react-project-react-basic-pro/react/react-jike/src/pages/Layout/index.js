ecc3ebf815e18f48d7a3b8a4f55c4244a4cf1ed5
乐天居易 <19836788252@163.com>
2024-06-01 17:13:41 +0800

    Layout-结构创建和样式reset

--- react-project-react-basic-pro/react/react-jike/src/pages/Layout/index.js
+++ react-project-react-basic-pro/react/react-jike/src/pages/Layout/index.js
@@ -1,14 +1,61 @@
-// 测试token是否成功注入
-import { request } from "@/utils"
-import { useEffect } from "react"
+import { Layout, Menu, Popconfirm } from 'antd'
+import {
+  HomeOutlined,
+  DiffOutlined,
+  EditOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons'
+import './index.scss'
+import 'normalize.css'
 
-function Layout(){
-    useEffect(()=>{
-        request.get('/user/profile')
-    },[])
-return(
-    <div>this is layout</div>
-)
+const { Header, Sider } = Layout
+
+const items = [
+  {
+    label: '首页',
+    key: '1',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: '文章管理',
+    key: '2',
+    icon: <DiffOutlined />,
+  },
+  {
+    label: '创建文章',
+    key: '3',
+    icon: <EditOutlined />,
+  },
+]
+
+const GeekLayout = () => {
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <div className="user-info">
+          <span className="user-name">柴柴老师</span>
+          <span className="user-logout">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+              <LogoutOutlined /> 退出
+            </Popconfirm>
+          </span>
+        </div>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            items={items}
+            style={{ height: '100%', borderRight: 0 }}></Menu>
+        </Sider>
+        <Layout className="layout-content" style={{ padding: 20 }}>
+          内容
+        </Layout>
+      </Layout>
+    </Layout>
+  )
 }
-
-export default Layout
+export default GeekLayout
