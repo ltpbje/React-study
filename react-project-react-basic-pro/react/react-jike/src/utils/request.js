3706bc10dc77246807517701f340bf8db866434d
乐天居易 <19836788252@163.com>
2024-06-02 16:47:15 +0800

    Home-Echarts基础图表渲染

--- react-project-react-basic-pro/react/react-jike/src/utils/request.js
+++ react-project-react-basic-pro/react/react-jike/src/utils/request.js
@@ -3,7 +3,8 @@
 //  3.请求拦截器/响应拦截器
 
 import axios from "axios";
-import { getToken } from "./token";
+import { getToken, removeToken } from "./token";
+import router from "@/router";
 
 const request = axios.create({
     baseURL:'http://geek.itheima.net/v1_0',
@@ -34,6 +35,11 @@ request.interceptors.response.use((response)=> {
   }, (error)=> {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    if(error.response.status === 401){
+      removeToken()
+      window.location.reload()
+      router.navigate('/login')
+    }
     return Promise.reject(error)
 })
 
