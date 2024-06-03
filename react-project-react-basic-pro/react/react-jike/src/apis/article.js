fcfd30433f301fcca628f1df22075c7c97afff91
乐天居易 <19836788252@163.com>
2024-06-03 16:20:09 +0800

    基础文章发布-收集表单数据提交表单

--- react-project-react-basic-pro/react/react-jike/src/apis/article.js
+++ react-project-react-basic-pro/react/react-jike/src/apis/article.js
@@ -11,3 +11,13 @@ export function getChannelAPI(){
         method:'GET'
     })
 }
+
+
+// 2.提交文章表单
+export function createArticleAPI(data){
+    return request({
+        url:'/mp/articles?draft=false',
+        method:'POST',
+        data
+    })
+}
--- react-project-react-basic-pro/react/react-jike/src/pages/Publish/index.js
+++ react-project-react-basic-pro/react/react-jike/src/pages/Publish/index.js
@@ -17,7 +17,7 @@ import './index.scss'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import { useEffect, useState } from 'react'
-import { getChannelAPI } from '@/apis/article'
+import { createArticleAPI, getChannelAPI } from '@/apis/article'
 
 
 const { Option } = Select
@@ -31,6 +31,25 @@ const Publish = () => {
         }
         getChannelList()
     },[])
+  
+
+  // 提交表单
+  const onFinish=(formValue)=>{
+    console.log(formValue)
+    const { title,content ,channel_id} = formValue
+    // 1．按照接口文档的格式处理收集到的表单数据
+    const reqData ={
+        title,
+        content,
+        cover:{
+            type:0,
+            images:[]
+        },
+        channel_id
+    }
+    // 2.调用接口提交
+    createArticleAPI(reqData)
+  }  
   return (
     <div className="publish">
       <Card
@@ -46,6 +65,7 @@ const Publish = () => {
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 1 }}
+          onFinish={onFinish}
         >
           <Form.Item
             label="标题"
