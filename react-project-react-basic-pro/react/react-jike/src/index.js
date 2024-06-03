99e47fbba6a586b6a617a669d248b439137d9bd6
乐天居易 <19836788252@163.com>
2024-06-03 09:03:01 +0800

    基础文章发布-准备富文本编辑器

--- react-project-react-basic-pro/react/react-jike/src/index.js
+++ react-project-react-basic-pro/react/react-jike/src/index.js
@@ -8,12 +8,10 @@ import { Provider } from 'react-redux';
 import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router}>
       </RouterProvider>
     </Provider>
-  </React.StrictMode>
 
 );
 
--- react-project-react-basic-pro/react/react-jike/src/pages/Publish/index.js
+++ react-project-react-basic-pro/react/react-jike/src/pages/Publish/index.js
@@ -13,6 +13,11 @@ import { PlusOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import './index.scss'
 
+
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+
+
 const { Option } = Select
 
 const Publish = () => {
@@ -52,7 +57,14 @@ const Publish = () => {
             label="内容"
             name="content"
             rules={[{ required: true, message: '请输入文章内容' }]}
-          ></Form.Item>
+          >
+            {/* 富文本编辑器 */}
+             <ReactQuill
+          className="publish-quill"
+          theme="snow"
+          placeholder="请输入文章内容"
+        />
+          </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 4 }}>
             <Space>
