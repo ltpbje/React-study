ef82b66673cedc7e0eaa88c8c924cfe9544fca4e
乐天居易 <19836788252@163.com>
2024-06-03 17:33:42 +0800

    文章封面-上传文章封面基础实现

--- react-project-react-basic-pro/react/react-jike/src/pages/Publish/index.js
+++ react-project-react-basic-pro/react/react-jike/src/pages/Publish/index.js
@@ -50,6 +50,11 @@ const Publish = () => {
     // 2.调用接口提交
     createArticleAPI(reqData)
   }  
+  const [imageList,setImageList] =useState([])
+  const onChange =(value)=>{
+    console.log('上传中',value)
+    setImageList(value.fileList)
+  }
   return (
     <div className="publish">
       <Card
@@ -74,6 +79,7 @@ const Publish = () => {
           >
             <Input placeholder="请输入文章标题" style={{ width: 400 }} />
           </Form.Item>
+
           <Form.Item
             label="频道"
             name="channel_id"
@@ -84,6 +90,31 @@ const Publish = () => {
               {channelList.map((item)=><Option key={item.id} value={item.id}>{item.name}</Option>)}  
             </Select>
           </Form.Item>
+
+          <Form.Item label="封面">
+                <Form.Item name="type">
+                    <Radio.Group>
+                    <Radio value={1}>单图</Radio>
+                    <Radio value={3}>三图</Radio>
+                    <Radio value={0}>无图</Radio>
+                    </Radio.Group>
+                </Form.Item>
+                {/* listType：决定选择文件框的外观样式
+                    showUpLoadList：控制显示上传列表 
+                */}
+                <Upload
+                    name="image"
+                    listType="picture-card"
+                    showUploadList
+                    action={'http://geek.itheima.net/v1_0/upload'}
+                    onChange={onChange}
+                >
+                    <div style={{ marginTop: 8 }}>
+                    <PlusOutlined />
+                    </div>
+                </Upload>
+          </Form.Item>
+
           <Form.Item
             label="内容"
             name="content"
