2eae26011385f59d968cdc4ec577c8f9ab829b90
乐天居易 <19836788252@163.com>
2024-06-06 17:25:05 +0800

    编辑文章-回填基础数据

--- react-project-react-basic-pro/react/react-jike/src/pages/Publish/index.js
+++ react-project-react-basic-pro/react/react-jike/src/pages/Publish/index.js
@@ -11,14 +11,14 @@ import {
   message
 } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import './index.scss'
 
 
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
-import { useState } from 'react'
-import { createArticleAPI } from '@/apis/article'
+import { useEffect, useState } from 'react'
+import { createArticleAPI, getArticleById } from '@/apis/article'
 import { useChannel } from '@/hooks/useChannel'
 
 
@@ -67,6 +67,25 @@ const Publish = () => {
     //  console.log('切换封面了',e.target.value)
      setImageType(e.target.value)
   }
+
+  // 回填数据
+  const [searchParams] = useSearchParams()
+  const articleId=searchParams.get('id')
+  // console.log(articleId)
+
+  // 获取form实例
+  const [form] = Form.useForm()
+  useEffect(()=>{
+    //1.通过id获取数据
+    //2、调用实例方法完成回填
+    async function getArticleDetail(){
+      const res = await getArticleById(articleId)
+      form.setFieldsValue(res.data)
+    }
+    getArticleDetail()
+
+  },[articleId,form])
+
   return (
     <div className="publish">
       <Card
@@ -83,6 +102,7 @@ const Publish = () => {
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 0 }}
           onFinish={onFinish}
+          form={form}
         >
           <Form.Item
             label="标题"
