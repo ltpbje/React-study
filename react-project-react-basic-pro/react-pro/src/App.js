5f73f12aca513a7948e4082153ac8603a7831303
乐天居易 <19836788252@163.com>
2024-06-08 16:16:47 +0800

    React.memo-props比较机制说明

--- react-project-react-basic-pro/react-pro/src/App.js
+++ react-project-react-basic-pro/react-pro/src/App.js
@@ -2,9 +2,12 @@
 
 
 // 1.传递一个简单类型的prop prop变化时组件重新渲染
-import { memo, useState } from "react";
+import { memo, useMemo, useState } from "react";
 
 // 2.传递一个引用类型的prop  比较的是新值和旧值的引用是否相等  当父组件的函数重新执行时，实际上形成的是新的数组引用
+
+
+// 3.保证引用稳定 -> useMemo 组件渲染的过程中缓存一个值
 const MemoSon = memo(
   function Son({list}){
     console.log('我是子组件 ,我重新渲染了');
@@ -20,8 +23,10 @@ const MemoSon = memo(
 function App() {
   const [count ,setCount] = useState(0)
   // const num = 0
-
-  const list =[1,2,3]
+  const list= useMemo(()=>{
+    return [1,2,3]
+  },[])
+  // const list  =[1,2,3]
   return (
     <div className="App">
       this is app
