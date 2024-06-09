8f07bfeead89751f38a9b64fe7c55feb9100defb
乐天居易 <19836788252@163.com>
2024-06-09 20:23:08 +0800

    zustand-异步支持

--- react-project-react-basic-pro/react-pro/src/App.js
+++ react-project-react-basic-pro/react-pro/src/App.js
@@ -1,5 +1,6 @@
+import { useEffect } from "react";
 import { create } from "zustand";
-
+const URL ='http://geek.itheima.net/v1_0/channels'
 // 1.创建store
 //语法容易出错
 // 1.函数参数必须返回一个对象对象内部编写状态数据和方法
@@ -13,16 +14,32 @@ const useStore= create((set)=>{
     // 修改状态数据的方法
     inc:()=>{
       set((state)=>({count:state.count + 1}))
+    },
+    channelList:[],
+    fetchGetList:async()=>{
+      const res= await fetch(URL)
+      const jsonRes = await res.json()
+      console.log(jsonRes)
+      set({channelList:jsonRes.data.channels})
     }
+
   }
 })
 // 2.绑定store
 // useStore => { count,inc 
 function App() {
-  const{count,inc} =useStore()
+  const{count,inc,fetchGetList,channelList} =useStore()
+  useEffect(()=>{
+    fetchGetList()
+  },[fetchGetList])
   return (
     <>
      <button onClick={()=>inc()}>{count}</button>
+     <ul>
+      {
+        channelList.map(item=><li key={item.id}>{item.name}</li>)
+      }
+     </ul>
     </>
   );
 }
