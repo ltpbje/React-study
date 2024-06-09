bc1122875e42005869665ff09f77dbb6252ebfdd
乐天居易 <19836788252@163.com>
2024-06-09 21:13:36 +0800

    zustand-切片模式

--- react-project-react-basic-pro/react-pro/src/11-zustand-异步支持.js
+++ react-project-react-basic-pro/react-pro/src/11-zustand-异步支持.js
@@ -2,34 +2,23 @@ import { useEffect } from "react";
 import { create } from "zustand";
 const URL ='http://geek.itheima.net/v1_0/channels'
 
-const createChannelStore=(set)=>{
-    return {  
-      channelList:[],
-      fetchGetList:async()=>{
-        const res= await fetch(URL)
-        const jsonRes = await res.json()
-        console.log(jsonRes)
-        set({channelList:jsonRes.data.channels})
-      }
-    }
-}
 
-
-const createCounterStore=(set)=>{
+const useStore= create((set)=>{
   return {
     // 状态数据
     count:0,
     // 修改状态数据的方法
     inc:()=>{
       set((state)=>({count:state.count + 1}))
+    },
+    channelList:[],
+    fetchGetList:async()=>{
+      const res= await fetch(URL)
+      const jsonRes = await res.json()
+      console.log(jsonRes)
+      set({channelList:jsonRes.data.channels})
     }
-  }
-}
 
-const useStore= create((...a)=>{
-  return {
-   ...createChannelStore(...a),
-   ...createCounterStore(...a)
   }
 })
 // 2.绑定store
